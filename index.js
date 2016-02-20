f9db01fbffcf6070411f041e2f97e0bc08cfdb73
Ignacio Esmite <iesmite@gmail.com>
2016-02-20 01:18:22 -0300

    Fixed saveFile arguments and attached this to private method

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
 var aws = require('aws-sdk');
 var fs = require('fs');
-var path = require('path');
 
-module.exports = function () {
-  store: storeFile,
-  awsCredentialsFromPath: function (path) {
-    aws.config.loadFromPath(path);
-  },
-  asS3: function() {
-    this.storage = 'S3'
-  }
-
-}
-
-function storeFile(name, imageContents, callback) {
+function storeFile(fileName, imageContents, callback) {
   var base64Data = imageContents.replace(/^data:image\/png;base64,/,"");
   var buff2Send = new Buffer(base64Data,'base64');
-  var jenkinsJob = process.env.JOB_NAME || 'JOB_NAME';
-  var buildId = process.env.BUILD_NUMBER || 'BUILD_NUMBER';
-  var name = jenkinsJob + "/" + buildId + "/" + title + '.png';
   if (this.storage === 'S3') {
-    uploadS3(fileName, bucket, fileContents, callback
+    uploadS3.call(this,fileName, buff2Send, callback);
   } else {
-    saveFile(path, fileName, fileContents, callback)
+    saveFile(fileName, buff2Send, callback);
   }
 
 }
 //callback: (err) =>
-function saveFile(filePath, fileName, fileContents, callback) {
-  fs.writeFile(path.join(filePath,fileName + ".png"), fileContents, 'base64', callback);
+function saveFile(fileName, fileContents, callback) {
+  fs.writeFile(fileName, fileContents, 'base64', callback);
 }
 
 //callback: (err) => (data)
-function uploadS3(fileName, bucket, fileContents, callback) {
+function uploadS3(fileName, fileContents, callback) {
   var s3 = new aws.S3();
   var params = {
-    Bucket: bucket,
-    Key: fileName
+    Bucket: this.bucket,
+    Key: fileName,
     Body: fileContents,
     ContentEncoding: 'base64',
     ContentType: 'image/png'
   };
   s3.putObject(params, callback);
+}
+
+module.exports = {
+  store: storeFile,
+  awsCredentialsFromPath: function (path) {
+    aws.config.loadFromPath(path);
+    return this;
+  },
+  asS3: function(bucket) {
+    this.storage = 'S3';
+    this.bucket = bucket;
+    return this;
+  }
+
 }
